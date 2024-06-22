5f6e61e2328a7eadc2f02399722b6dcf8b0f4b67
wethebest000 <wethebest000@yahoo.com>
2024-06-22 13:09:28 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number to the variable n each time it is executed.
+ *              It prints whether the number stored in the variable n is positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n == 0)
+    {
+        printf("%d is zero\n", n);
+    }
+    else
+    {
+        printf("%d is negative\n", n);
+    }
+
+    return (0);
+}
+
