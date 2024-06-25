d79e4d38c70a5fc82813e33446c19ac68db70b11
wethebest000 <wethebest000@yahoo.com>
2024-06-25 08:17:02 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,9 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints "_putchar" followed by a new line
+ * Description:
+ * Prints "_putchar", followed by a new line, using the _putchar function.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
