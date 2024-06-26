9a7f37d95fbfbd69eb8e5a701aff37c0a331bb6d
wethebest000 <wethebest000@yahoo.com>
2024-06-26 15:07:09 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,15 +3,15 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
- 
+ *
  * Return: 1 if for alphabetic character or 0 for anything else
  */
-	int _isalpha(int c)
-	{	
+int _isalpha(int c)
+{
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-	return 1;
+		return (1);
 	}
-	return 0;
-	}
+	return (0);
+}
 
