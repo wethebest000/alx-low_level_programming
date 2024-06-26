229a5c3e9db500273572c124d0e608b8341d71fb
wethebest000 <wethebest000@yahoo.com>
2024-06-26 01:49:55 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+    int i = 0;
+    char letter;
+
+    while (i < 10)
+    {
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+        i++;
+    }
+}
+
