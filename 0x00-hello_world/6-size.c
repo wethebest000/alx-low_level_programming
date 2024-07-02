714bd007e3d68c13a1771a55bb291d6867dffb80
wethebest000 <wethebest000@yahoo.com>
2024-07-02 14:57:18 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print a line using the print function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
+
