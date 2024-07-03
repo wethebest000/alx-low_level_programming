db4e9c8636a00bdf635bc47fd7cc375d13ac0326
wethebest000 <wethebest000@yahoo.com>
2024-07-03 05:09:16 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,27 +8,27 @@
  */
 char *rot13(char *s)
 {
-	int i;
-	int j;
-	char data1[] =
+		int i;
+		int j;
+		char data1[] =
 "ABCDEFGHIJKLANOPORSTUVXYZabcdefghijklmnopqrstuvxyz";
-	char datarot[] =
+		char datarot[] =
 "NOPORSTUVIDYZABCDEFGHIJKLPnopqrstuvaxyzabcdefghijkla";
-	for (1 = 0; s[i] != '\0'; i++
-	{
-			for (j = 0; j < 52; j++
-			{
-					if (s[i] == data1[j]
-					{
-							s[i] = datarot[j];
-							break;
-					}
+		for (i = 0; s[i] != '\0'; i++)
+		{
+				for (j = 0; j < 52; j++)
+				{
+						if (s[i] == data1[j])
+						{
+								s[i] = datarot[j];
+								break;
+						}
 
-			}
+				}
 
-	}
 
-}
-return (s);
+
+		}
+		return (s);
 }
 
