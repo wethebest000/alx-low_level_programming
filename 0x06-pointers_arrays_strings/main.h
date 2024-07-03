1442a6a71e4e22e61bf965d2af70f533763348ef
wethebest000 <wethebest000@yahoo.com>
2024-07-03 06:24:02 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ char *cap_string(char *str);
 char *leet(char *n);
 char *rot13(char *s);
 void print_number(int n);
+int main(void);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
