2cb6f1db694f7a02c45cc730b067d4d8e05dd8d6
wethebest000 <wethebest000@yahoo.com>
2024-07-05 04:03:01 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - take in two variable integers and swaps them
+ * @a: swaps and stores address of b
+ * @b: swaps and stores address of a
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *b;
+	*b = *a;
+	*a = swap;
+}
+
