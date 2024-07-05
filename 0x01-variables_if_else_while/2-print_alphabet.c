8652c3f52e991aae87f02161790ed74934b34d42
wethebest000 <wethebest000@yahoo.com>
2024-07-05 13:51:49 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,20 +4,20 @@
  * main - Entry point
  *
  * Description: Prints the alphabet in lowercase followed by a new line.
- *              You can only use putchar twice in your code.
+ * You can only use putchar twice in your code.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int letter;
+	int letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
