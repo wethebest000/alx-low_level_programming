9fc334e6b3caf72102196c2887259c8fb1a7a7b2
wethebest000 <wethebest000@github.com>
2024-07-18 18:49:46 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
+
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
- */
+  * _strlen_recursion - Calculate the length of a string
+  * @s: the string to count
+  *
+  * Return: integer value
+  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-	else
-	{
-		return 1 + _strlen_recursion(s + 1);
-	}
-}
 
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
