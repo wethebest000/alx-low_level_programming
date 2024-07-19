c80c1f299ec3c1dd330101762ab047d35cc6723b
wethebest000 <wethebest000@github.com>
2024-07-18 18:33:06 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * pow_recursion - Returns the value of x raised to the power of y.
- * @x: The base value.
- * @y: The exponent value.
- *
- * Return: If y is negative, the function returns -1 (error).
- * If y is 0, the function returns 1 (base case).
- * Otherwise, it returns x raised to the power of y.
- */
-int pow_recursion(int x, int y)
+  * _pow_recursion - Returns the value of x raised to the power of y
+  * @x: the value to multiply
+  * @y: the times to multiply the value
+  *
+  * Return: the value multiplied y times
+  */
+int _pow_recursion(int x, int y)
 {
-		if (y < 0)
-			return (-1);
-		if (y == 0)
-			return (1);
-	return (x * pow_recursion(x, y - 1));
-}
+	if (y < 0)
+		return (-1);
 
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
